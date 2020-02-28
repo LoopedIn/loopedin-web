8d5ee32a99dfcc54e499704275e9ec0e58e09665
suhan0694 <snath6@uic.edu>
2020-02-27 19:51:00 -0600

    Workflow changes

--- server/lib/server.js
+++ server/lib/server.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
-const cors = require("cors");
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const debug = require("debug")("homework1-server");
-const http = require("http");
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const debug = require('debug')('homework1-server');
+const http = require('http');
 
 const app = express();
 const router = express.Router();
@@ -34,8 +34,8 @@ function normalizePort(val) {
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 /**
  * Create HTTP server.
@@ -48,19 +48,19 @@ const server = http.createServer(app);
  */
 
 function onError(error) {
-  if (error.syscall !== "listen") {
+  if (error.syscall !== 'listen') {
     throw error;
   }
 
-  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case "EACCES":
+    case 'EACCES':
       console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
-    case "EADDRINUSE":
+    case 'EADDRINUSE':
       console.error(`${bind} is already in use`);
       process.exit(1);
       break;
@@ -75,7 +75,7 @@ function onError(error) {
 
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
   debug(`Listening on ${bind}`);
 }
 
@@ -84,5 +84,5 @@ function onListening() {
  */
 
 server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
