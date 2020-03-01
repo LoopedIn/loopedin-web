ad1f4a946b49598aeca92d86cd6b79552e5c86d2
suhan0694 <snath6@uic.edu>
2020-02-29 19:39:24 -0600

    React Login and Sigup page

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React from "react";
+import Register from "../register/register";
+import LoginSide from "../login/login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
- render() {
-   return (
-     <div>
-        <h1>Test React App</h1>
-     </div>
-   );
- }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LoginSide />
+          </Route>
+          <Route path="/login">
+            <LoginSide />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
