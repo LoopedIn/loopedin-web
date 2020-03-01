f5817942d5e9d9e12ee3179eae8b7a3c0ff9b173
Krunal Bhatt <krunal.bhatt9@gmail.com>
2020-02-29 19:31:54 -0600

    Revert "Added firebase login, logout,verifyuser functions, libraries and other utility functions"

--- client/src/utilities/utilities.js
+++
@@ -1,7 +0,0 @@
-const validateLoginCredentials = (userName, password) => {
-  if (userName === undefined || userName.length === 0 || password.length < 8) {
-    return false;
-  }
-
-  return true;
-};
