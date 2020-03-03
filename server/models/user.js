5a26ae24bd5192aa63a85c23befbc13f8114a8d7
Shiva Reddy <kjshiva94@gmail.com>
2020-03-02 19:16:38 -0600

    added stubs

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const user = mongoose.Schema({
-  passwordHash: { type: String, required: true },
+  userId: {type: String, required: true, unique: true},
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  tokenSeed: { type: String, required: true, unique: true },
+  firstName: {type: String, required: true, unique: false},
+  lastName: { type: String, required: true, unique: false },
   created: { type: Date, default: () => new Date() }
 });
 const userSchema = mongoose.model("user", user);
