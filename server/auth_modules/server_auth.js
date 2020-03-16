cd3bf80025cd122e440f2189516772bce6e2afd7
Shiva Reddy <kjshiva94@gmail.com>
2020-03-15 22:30:03 -0500

    using github secret as firebase auth key source

--- server/auth_modules/server_auth.js
+++ server/auth_modules/server_auth.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require("../config/serviceAccountKey.json");
+
+const serviceAccount = process.env.FIREBASE_SECRET?  process.env.FIREBASE_SECRET : require("../config/serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
