a0339218dd9c75aa5a39e2888c3804b32a8f0d6a
Suhan Nath <snath6@uic.edu>
2020-03-24 17:47:23 -0500

    :lipstick: Home Screen Boiler Plate with Appbar, Navbar

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,47 +1,221 @@
-import React, { Component, useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { logoutUser, getUserFriends, getUserLoops } from "../../actions";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Box from "@material-ui/core/Box";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import { mainListItems, secondaryListItems } from "./dashboard-items";
+import { APP_TITLE, APP_COPYRIGHT } from "../../utils/ui-constants";
 
-const Home = (props) => {
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {APP_COPYRIGHT}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-  const { 
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: 36
+  },
+  menuButtonHidden: {
+    display: "none"
+  },
+  title: {
+    flexGrow: 1
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9)
+    }
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto"
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column"
+  },
+  fixedHeight: {
+    height: 240
+  }
+}));
+
+const Home = props => {
+  const {
     //Objects retrieved from state
-    isLoggingOut, logoutError,logoutUser,
-    user, loops, friends,
+    isLoggingOut,
+    logoutError,
+    logoutUser,
+    user,
+    loops,
+    friends,
 
     //Actions to run on page load
-    getUserFriends,getUserLoops
+    getUserFriends,
+    getUserLoops
   } = props;
 
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
   //Runs getUserFriends and getUserloops when component is loaded
-  useEffect(() =>  {
-    getUserFriends()
-  }, [getUserFriends])
+  useEffect(() => {
+    getUserFriends();
+  }, [getUserFriends]);
 
-  useEffect(() =>  {
-    getUserLoops()
-  }, [getUserLoops])
+  useEffect(() => {
+    getUserLoops();
+  }, [getUserLoops]);
 
-  
   return (
     <div>
-      <Typography component="h1" variant="h3">
-        LoopedIn
-      </Typography>
-      <Typography component="h1" variant="h5">
-        {user.firstName} has logged in! All routes are protected until you Logout
-      </Typography>
-      <Typography component="h1" variant="h5">
-        {loops}
-      </Typography>
-      <Typography component="h1" variant="h5">
-        {friends}
-      </Typography>
-      <Button onClick={logoutUser}>Logout</Button>
-      {isLoggingOut && <p>Logging Out....</p>}
-      {logoutError && <p>Error logging out</p>}
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="absolute"
+          className={clsx(classes.appBar, open && classes.appBarShift)}
+        >
+          <Toolbar className={classes.toolbar}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              className={clsx(
+                classes.menuButton,
+                open && classes.menuButtonHidden
+              )}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={classes.title}
+            >
+              {APP_TITLE}
+            </Typography>
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose)
+          }}
+          open={open}
+        >
+          <div className={classes.toolbarIcon}>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>{mainListItems}</List>
+          <Divider />
+          <List onClick={logoutUser}>{secondaryListItems}</List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="lg" className={classes.container}>
+            <Box pt={4}>
+              {isLoggingOut && <p>Logging Out....</p>}
+              {logoutError && <p>Error logging out</p>}
+              <Copyright />
+            </Box>
+          </Container>
+        </main>
+      </div>
     </div>
   );
 };
@@ -56,4 +230,8 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, { logoutUser, getUserFriends, getUserLoops })(Home);
+export default connect(mapStateToProps, {
+  logoutUser,
+  getUserFriends,
+  getUserLoops
+})(Home);
