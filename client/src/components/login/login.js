29fd273fbee86aa97c1d27fb9313b1ff94b7545b
Suhan Nath <snath6@uic.edu>
2020-03-29 01:54:44 -0500

    :lipstick: UI Boiler plate redesign with NavBar

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
@@ -15,7 +12,7 @@ import { makeStyles, withStyles } from "@material-ui/core/styles";
 import { Link as RouterLink } from "react-router-dom";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
-import { loginUser } from "../../actions/";
+import { loginUser } from "../../actions";
 
 function Copyright() {
   return (
@@ -100,7 +97,6 @@ export const loginSide = props => {
   } else {
     return (
       <Grid container component="main" className={classes.root}>
-        <CssBaseline />
         <Grid item xs={false} sm={4} md={7} className={classes.image} />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <div className={classes.paper}>
