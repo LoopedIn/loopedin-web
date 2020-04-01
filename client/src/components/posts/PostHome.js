049c8814be80ecdd31e436bf53848d041778cb28
Suhan Nath <snath6@uic.edu>
2020-04-01 03:13:20 -0500

    :liptstick: UI for Posts Lists

--- client/src/components/posts/PostHome.js
+++ client/src/components/posts/PostHome.js
@@ -12,6 +12,9 @@ const PostHome = props => {
         <Grid item xs={12} md={6}>
           <Posts />
         </Grid>
+        <Grid item xs={12} md={6}>
+          <PostLists />
+        </Grid>
       </Grid>
     </Fragment>
   );
