390291486290e55ff7ea9d13dddbcabb66a27f25
Shiva Reddy <kjshiva94@gmail.com>
2020-04-04 18:18:30 -0500

    Revert hardcoded changes

--- server/auth_modules/server_auth.js
+++ server/auth_modules/server_auth.js
@@ -9,29 +9,29 @@ admin.initializeApp({
   databaseURL: "https://loopedin-269607.firebaseio.com"
 });
 
-// async function tokenIdToDbUser (tokenUid) {
-//     return await User.findOne({authToken:tokenUid})
-// }
+async function tokenIdToDbUser (tokenUid) {
+    return await User.findOne({authToken:tokenUid})
+}
 
 const firebaseTokenAuthenticator = async (req, res, next) => {
-  const user = await User.findOne({authToken: "Umm9sECtpPUuWjPeUSwRsrAAzYg1"})
-  req.body.userID = user._id
-  req.body.userId= user._id
-  req.body.user = user
-  next()
-  // if (req.body.idToken) {
-  //    admin.auth().verifyIdToken(req.body.idToken)
-  //     .then(async (decodedToken) => {
-  //       const user = await tokenIdToDbUser(decodedToken.uid);
-  //       req.body.userID = user._id
-  //       req.body.user = user
-  //       next()
-  //     }).catch(() => {
-  //       res.status(403);
-  //     });
-  // } else {
-  //   res.status(403).send();
-  // }
+  // const user = await User.findOne({authToken: "Umm9sECtpPUuWjPeUSwRsrAAzYg1"})
+  // req.body.userID = user._id
+  // req.body.userId= user._id
+  // req.body.user = user
+  // next()
+  if (req.body.idToken) {
+     admin.auth().verifyIdToken(req.body.idToken)
+      .then(async (decodedToken) => {
+        const user = await tokenIdToDbUser(decodedToken.uid);
+        req.body.userID = user._id
+        req.body.user = user
+        next()
+      }).catch(() => {
+        res.status(403);
+      });
+  } else {
+    res.status(403).send();
+  }
 }
 
 module.exports = {firebaseTokenAuthenticator};
